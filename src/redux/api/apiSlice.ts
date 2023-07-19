8c14ffe6e81ef0c52df79cf07a4feb1b53d60c64
zahidmiran <zahidmiron599@gmail.com>
2023-07-19 23:24:45 +0600

    all set

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://book-vault-server.vercel.app/",
-    baseUrl: "http://localhost:3000/",
+    // baseUrl: "http://localhost:3000/",
+    baseUrl: "https://bookish-server-main-jawadjisan.vercel.app/",
   }),
   tagTypes: ["Books", "Book", "Reviews", "Wishlist", "Booklist"],
   endpoints: () => ({}),
